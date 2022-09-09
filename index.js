01c0147eaf5adf3274deb3a34b15eb0724c4b15a
mwendamwenda <benmwenda83@gmail.com>
2022-09-09 06:31:52 +0300

    adds description to the cars

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const carsData = [
   {
       "image":"https://cdn.luxe.digital/media/2021/11/26104936/best-classic-cars-vintage-old-luxe-digital%402x-1536x768.jpg.webp" ,
       "tittle":"Ferrari 250 GTO 1962",
-      "descriptipn": "The sleek, stylish Ferrari 250 GTO is one of the most famous classic cars of all time and for a very good reason.",
+      "description": "The sleek, stylish Ferrari 250 GTO is one of the most famous classic cars of all time and for a very good reason.",
       "price": "9,99"
 
   },
@@ -16,7 +16,7 @@ const carsData = [
   {
       "image":"https://cdn.luxe.digital/media/2021/11/26104949/best-classic-cars-vintage-Porsche-911-1963-old-luxe-digital%402x.jpg.webp" ,
       "tittle":"Porsche 911 1963",
-      "descriptipn": "The Porsche 911 is described as a nearly perfect classic sports car—because there’s always room for a little bit of improvement. A long-time staple of the classic car community, this universally loved vehicle takes plenty of prizes when it comes to impressive statistics.",
+      "description": "The Porsche 911 is described as a nearly perfect classic sports car—because there’s always room for a little bit of improvement. A long-time staple of the classic car community, this universally loved vehicle takes plenty of prizes when it comes to impressive statistics.",
       "price": "9,99"
 
   },
@@ -37,7 +37,7 @@ const carsData = [
   {
       "image":"image5" ,
       "tittle":"British Motor Corporation Mini 1959",
-      "descriptipn": "There’s no classic car quite like the classic Mini. Modernized time and time again with new colors and features, this is the classic car you’re most likely to still see in action in the 21st century. Shortly after its initial release, the Mini quickly became one of the most popular classic cars of all time.",
+      "description": "There’s no classic car quite like the classic Mini. Modernized time and time again with new colors and features, this is the classic car you’re most likely to still see in action in the 21st century. Shortly after its initial release, the Mini quickly became one of the most popular classic cars of all time.",
       "price": "9,99"
 
   },
@@ -45,28 +45,28 @@ const carsData = [
       "image":"https://cdn.luxe.digital/media/2021/11/26104902/best-classic-cars-vintage-Ford-Mustang-Shelby-GT350-1965-old-luxe-digital%402x.jpg.webp" ,
       "tittle":"Ford Mustang Shelby GT350 1965",
       
-      "descriptipn": "The Shelby Mustang was designed to look good and to drive fast. It wasn’t specially designed for comfort, and it definitely wasn’t designed for family days out. But at least it excelled in the area it originally chose.",
+      "description": "The Shelby Mustang was designed to look good and to drive fast. It wasn’t specially designed for comfort, and it definitely wasn’t designed for family days out. But at least it excelled in the area it originally chose.",
       "price": "1,399"
 
   },
   {
       "image":"https://cdn.luxe.digital/media/2021/11/26104932/best-classic-cars-vintage-Mercedes-300SL-Gullwing-1954-old-luxe-digital%402x.jpg.webp" ,
       "tittle":"Mercedes 300SL Gullwing 1954",
-      "descriptipn": "Mention the Mercedes 300SL Gullwing 1954 and you’ll undoubtedly see ears prick up, especially if you’re in a circle of classic car fans. One of the most stylish and iconic classic cars in the world, the Gullwing firmly cemented its place as a premium offering as soon as it launched.",
+      "description": "Mention the Mercedes 300SL Gullwing 1954 and you’ll undoubtedly see ears prick up, especially if you’re in a circle of classic car fans. One of the most stylish and iconic classic cars in the world, the Gullwing firmly cemented its place as a premium offering as soon as it launched.",
       "price": "9,99"
 
   },
   {
       "image":"https://cdn.luxe.digital/media/2021/11/26104831/best-classic-cars-vintage-Chevrolet-Corvette-1963-old-luxe-digital%402x.jpg.webp" ,
       "tittle":"Chevrolet Corvette 1963",
-      "descriptipn": "The Chevrolet Corvette 1963 was a rare car even when it was first released, which means sourcing a journey in one of these nowadays—let alone even trying to buy one—is an adventure in itself.",
+      "description": "The Chevrolet Corvette 1963 was a rare car even when it was first released, which means sourcing a journey in one of these nowadays—let alone even trying to buy one—is an adventure in itself.",
       "price": "9,99"
 
   },
   {
       "image":"image9" ,
       "tittle":"https://cdn.luxe.digital/media/2021/11/26104827/best-classic-cars-vintage-Bugatti-Type-57-Atlantic-1938-old-luxe-digital%402x.jpg.webp",
-      "descriptipn": "Thought an older Bugatti might be more budget-friendly than a recent model? Think again. You need no better proof that the 1938 Bugatti Type 57 Atlantic will ever go out of fashion than by the knowledge that Ralph Lauren owns the very last one to ever be produced. First created as an entirely new design by Jean Bugatti, there were only ever 710 of these models produced. ",
+      "description": "Thought an older Bugatti might be more budget-friendly than a recent model? Think again. You need no better proof that the 1938 Bugatti Type 57 Atlantic will ever go out of fashion than by the knowledge that Ralph Lauren owns the very last one to ever be produced. First created as an entirely new design by Jean Bugatti, there were only ever 710 of these models produced. ",
       "price": "9,99"
 
   }
@@ -80,6 +80,8 @@ ${carsData.map(function(car){
   return `
   <div class="machines">
     <img class="car-photo" src="${car.image}">
+    <h2>${car.tittle} (${car.price})</h2>
+    <h3>${car.description}</h3>
   </div>
   `
 
